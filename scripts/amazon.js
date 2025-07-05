5159948fbaf53fa85cdc2616a9948ce734434fcb
Tobechi <tobechi.duru001@gmail.com>
2025-07-04 20:07:39 -0700

    updated amazon.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,62 +1,27 @@
-// const products = [{
-//       image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-//       name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//       rating: {
-//         star: 4.5,
-//         count: 87
-//       },
-//       priceCents: 1090
-//     },  {
-//       image: 'images/products/intermediate-composite-basketball.jpg',
-//       name: 'Intermediate Size Basketball',
-//       rating: {
-//         star: 4,
-//         count: 127
-//       },
-//       priceCents: 2095
-//     },  {
-//       image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//       name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-//       rating: {
-//         star: 4.5,
-//         count: 56
-//       },
-//       priceCents: 799
-//     },  {
-//       image: 'images/products/black-2-slot-toaster.jpg',
-//       name: '2 Slot Toaster - Black',
-//       rating: {
-//         star: 5,
-//         count: 2197
-//       },
-//       priceCents: 1899
-//     }, 
-// ];
+let productHTML = '';
 
-
-let productsHTML = '';
-
-products.forEach((product) =>{
-  productsHTML += ` <div class="product-container">
+products.forEach((products) => {
+  
+  productHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.image}">
+              src="${products.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${product.name}
+            ${products.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="images/ratings/rating-${products.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
+              ${products.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-             $${(product.priceCents / 100).toFixed(2)}
+            $${(products.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -82,11 +47,9 @@ products.forEach((product) =>{
           </div>
 
           <button class="add-to-cart-button button-primary">
-            Add to Cart 
+            Add to Cart
           </button>
-        </div>`;
-});
+        </div>`
+})
 
-
-document.querySelector('.js-product-grid').innerHTML = productsHTML;
-
+document.querySelector('.js-product-grid').innerHTML = productHTML;
