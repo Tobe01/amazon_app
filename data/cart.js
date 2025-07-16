6ecb2f39e7f78ab6fd0bf7fc6bfd74c1aa7a7723
Tobechi <tobechi.duru001@gmail.com>
2025-07-16 14:09:14 -0700

    Used modules

--- data/cart.js
+++ data/cart.js
@@ -1,27 +1,28 @@
 export const cart = [];
-
 export function addToCart(productId){
+// make matchingItem undefined
+  let matchingItem;  
 
-    // make matchingItem undefined
-    let matchingItem;  
-
-    /* Loop through the cart to check if product name is available
-    in each, and save in matching Item variable
-    */
-    cart.forEach((item) => {
-      if(productId === item.productId)
-      matchingItem = item;
+  /* Loop through the cart to check if product name is available
+  in each, and save in matching Item variable
+  */
+  cart.forEach((item) => {
+    if(productId === item.productId)
+    matchingItem = item;
+  })
+  
+  /* if product name is available. add it to matching item, and increase the quantity by 1
+    else, push a new items into the cart
+  */
+  if(matchingItem){
+    matchingItem.quantity += 1;
+  } else{
+      cart.push({
+      productId: productId,
+      quantity: 1
     })
-    
-    /* if product name is available. add it to matching item, and increase the quantity by 1
-     else, push a new items into the cart
-    */
-    if(matchingItem){
-      matchingItem.quantity += 1;
-    } else{
-        cart.push({
-        productId: productId,
-        quantity: 1
-      })
-    }
+  }
 }
+
+
+
