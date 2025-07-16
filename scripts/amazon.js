c5a59fb264b72e9c9236a4373213bbfdf5695d96
Tobechi <tobechi.duru001@gmail.com>
2025-07-16 16:09:45 -0700

    delete product from cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 addToCart();
 let productHTML = '';
@@ -25,7 +26,7 @@ productHTML += `<div class="product-container">
   </div>
 
   <div class="product-price">
-    $${(products.priceCents / 100).toFixed(2)}
+    $${formatCurrency(products.priceCents)}
   </div>
 
   <div class="product-quantity-container">
