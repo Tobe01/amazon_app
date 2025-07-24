316ab18dda081ad8f049b857d52392cbf24f59ce
Tobechi <tobechi.duru001@gmail.com>
2025-07-24 15:53:54 -0700

    Updated Payment Summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -10,4 +10,16 @@ export let deliveryOptions = [{
   id: '1',
   deliveryDays: 1,
   priceCents: 999
-}] 
+}];
+
+export function getDeliveryOption(deliveryOptionId){
+   let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+      if(option.id === deliveryOptionId){
+        deliveryOption = option;
+      }
+    });
+
+  return deliveryOption || deliveryOptions[0];
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 
 hello();
 
@@ -19,13 +19,8 @@ export function renderOrderSummary(){
   cart.forEach((cartItem) => {
   
     const productId = cartItem.productId;
-    let matchingProduct;
 
-    products.forEach((product) => {
-      if(product.id === productId){
-        matchingProduct = product;
-      }
-    })
+    const matchingProduct = getProduct(productId);
 
     // Check if matchingProduct was found before trying to use it
     if (matchingProduct === undefined) {
@@ -35,13 +30,7 @@ export function renderOrderSummary(){
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionId){
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
         const correctDate = today.add(deliveryOption.deliveryDays, 'days');
@@ -188,4 +177,6 @@ export function renderOrderSummary(){
     })
   });
 };
+
+
 
