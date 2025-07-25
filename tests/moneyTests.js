c6f0e2cf208ba1cce144ae5867ccb9027c6692d0
Tobechi <tobechi.duru001@gmail.com>
2025-07-24 18:11:23 -0700

    Automatic testing

---
+++ tests/moneyTests.js
@@ -0,0 +1,19 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('Test suit: Format currency');
+
+console.log('converts cents to dollars');
+
+if (formatCurrency(2095) === '20.95'){
+  console.log('Passed');
+} else {
+  console.log('failed');
+};
+
+console.log('works with zero');
+
+if (formatCurrency(0) === '0.00'){
+  console.log('Passed');
+} else {
+  console.log('failed');
+};
