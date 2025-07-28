4de1cb89e3e4c4bfcf92cebe0cf085601df21ced
Tobechi <tobechi.duru001@gmail.com>
2025-07-28 16:00:33 -0700

    Updates

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,1 +1,12 @@
-import {addToCart} from '../../data/cart.js';
+import {addToCart, cart} from '../../data/cart.js';
+
+describe('Test suit: add to cart', ()=>{
+  it('adds a new product to the cart', ()=> {
+
+  })
+
+  it('updates the cart with an exixting product', ()=>{
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expects(cart.length()).toEqual(1);
+  });
+});
--- test-jasmine/utils/moneyTest.js
+++ test-jasmine/utils/moneyTest.js
@@ -9,4 +9,8 @@ describe('Test suit: Format currency', ()=> {
   it('works with zero', ()=> {
     expect(formatCurrency(0)).toEqual('0.00');
   });
+
+  it('rounds up to the nearest cent', ()=> {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
 });
