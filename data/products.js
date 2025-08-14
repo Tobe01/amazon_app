54e23b55659c20d8276a0a386e4761d0e417fb3b
Tobechi <tobechi.duru001@gmail.com>
2025-08-14 15:33:36 -0700

    Updates to project

--- data/products.js
+++ data/products.js
@@ -36,6 +36,32 @@ class product {
     return ` $${formatCurrency(this.priceCents)}`
   }
 }
+class Clothing extends product(){
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+}
+
+const tshirt = new Clothing ({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+});
 
 
 export const products = [
