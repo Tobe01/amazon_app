457743fa519ca2a59f124c198c1b063a329e76f2
Tobechi <tobechi.duru001@gmail.com>
2025-08-20 17:52:29 -0700

    code updates

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,27 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/backend-practice.js';
 // import '../data/cart-class.js';
 
-loadProducts(()=> {
- renderOrderSummary();
- renderPaymentSummary();
+loadCart();
+
+
+Promise.all([
+  new Promise((resolve)=> {
+    loadProducts(()=> {
+      resolve('value 1');
+    })
+  }),
+
+  new Promise((resolve)=> {
+    loadCart(()=> {
+      resolve('Value 2');
+    })
+  })
+]). then((value)=> {
+   console.log(value);
+   renderOrderSummary();
+   renderPaymentSummary();
 });
